de738ec6b290568ea3750f918c975f6caebeb282
Hecz Klaudia <kicsihold21@gmail.com>
2021-04-28 09:28:54 +0200

    js globális változók

--- public_html/memoriajatek.js
+++ public_html/memoriajatek.js
@@ -1,23 +1,3 @@
-$(function () {
-    $("article").html("Helo");
-    $("article").append("<img>");
-//    $("article img").attr("src", "kepek/...");
-    $("article img").eq(0).attr("src", kepek[0].eleresiUt);
-//    $("article img").attr("alt", "valami");
-    $("article img").eq(0).attr("alt", kepek[0].alt);
-    $("article").append("<img>");
-    $("article img").eq(1).attr("src", kepek[1].eleresiUt);
-    $("article img").eq(1).attr("alt", kepek[1].alt);
-    $("article img").click(kattintasra);
-
-
-});
-
-for (var i = 0; i < kepek.length; i++) {
-    var elem = '<div>' + i + '</div>';
-    $("article").append(elem);
-}
-
 var kep1 = {
     eleresiUt: "kepek/....jpg",
     alt: "kép címe"
@@ -29,6 +9,43 @@ var kep2 = {
 };
 
 var kepek = [kep1, kep2]; //fontos hogy mögötte legyen
+
+$(function () {
+//    $("article").html("Helo");
+//    $("article").append("<img>");
+////    $("article img").attr("src", "kepek/...");
+//    $("article img").eq(0).attr("src", kepek[0].eleresiUt);
+////    $("article img").attr("alt", "valami");
+//    $("article img").eq(0).attr("alt", kepek[0].alt);
+//    $("article").append("<img>");
+//    $("article img").eq(1).attr("src", kepek[1].eleresiUt);
+//    $("article img").eq(1).attr("alt", kepek[1].alt);
+//    $("article img").click(kattintasra);
+
+
+});
+
+for (var i = 0; i < kepek.length; i++) {
+    var elem = '<div>' + i + '</div>';
+    $("article").append(elem);
+}
+
+    var kepElemTomb = $("article div img");
+    var cimElemTomb = $("article div h3");
+    var leirasElemTomb = $("article div p");
+
+    for (var i = 0; i < kepElemTomb.length; i++) {
+//        console.log(kepTomb[i].eleresiut);
+        kepElemTomb.eq(i).attr("src", kepek[i].eleresiut);
+        cimElemTomb.eq(i).html(kepek[index].cim);
+        leirasElemTomb.eq(i).html(kepek[index].leiras);
+    }
+
+    //kattintás eseménykezelő a kisképre
+    for (var i = 0; i < kepElemTomb.length; i++) {
+        kepElemTomb[i].click(kepCsere);
+    }
+
 
 function kattintasra() {
     $(this).attr("src", "kepek/...");
