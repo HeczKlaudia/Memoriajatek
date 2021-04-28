4d4794840a5bc71dfb88b10385803ecca2d566aa
Hecz Klaudia <kicsihold21@gmail.com>
2021-04-28 08:45:38 +0200

    létrehozás

---
+++ public_html/memoriajatek.js
@@ -0,0 +1,31 @@
+$(function () {
+    $("article").html("Helo");
+    $("article").append("<img>");
+//    $("article img").attr("src", "kepek/...");
+    $("article img").eq(0).attr("src", kepek[0].eleresiUt);
+//    $("article img").attr("alt", "valami");
+    $("article img").eq(0).attr("alt", kepek[0].alt);
+    $("article").append("<img>");
+    $("article img").eq(1).attr("src", kepek[1].eleresiUt);
+    $("article img").eq(1).attr("alt", kepek[1].alt);
+    $("article img").click(kattintasra);
+
+
+});
+
+var kep1 = {
+    eleresiUt: "kepek/....jpg",
+    alt: "kép címe"
+};
+
+var kep2 = {
+    eleresiUt: "kepek/....jpg",
+    alt: "kép címe"
+};
+
+var kepek = [kep1, kep2]; //fontos hogy mögötte legyen
+
+function kattintasra() {
+    $(this).attr("src", "kepek/...");
+    $(this).attr("alt", "valami");
+}
